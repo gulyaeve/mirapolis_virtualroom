9d15bf28dd72be7635e7d8f833083b922048c960
gulyaeve <gulyaeve@icloud.com>
2024-07-24 23:09:56 +0300

    Added models and parameters offset, limit

--- mirapolis_virtualroom/virtualroom_api.py
+++ mirapolis_virtualroom/virtualroom_api.py
@@ -1,4 +1,5 @@
 from .base_api import BaseAPI
+from .models import Measure
 
 
 class VirtualRoom(BaseAPI):
@@ -14,19 +15,29 @@ class VirtualRoom(BaseAPI):
             app_id
         )
 
-    async def get_measures(self):
+    async def get_persons(self, limit: int = 200, offset: int = 0):
+        persons = await self.get_json(
+            route="/service/v2/persons",
+            params={
+                "limit": limit,
+                "offset": offset,
+            }
+        )
+        return persons
+
+    async def get_measures(self, limit: int = 200, offset: int = 0):
         measures = await self.get_json(
             route="/service/v2/measures",
             params={
-                "limit": "200",
-                # "offset": "20"
+                "limit": limit,
+                "offset": offset,
             }
         )
-        return measures
+        if measures:
+            return [Measure(**measure) for measure in measures]
 
     async def get_measures_info(self):
         measures_info = await self.get_json(
             route="/service/v2/measures/info"
         )
         return measures_info
-
